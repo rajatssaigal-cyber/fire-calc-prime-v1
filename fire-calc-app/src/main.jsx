d1aaa9085351f2653ec1539c092834de59588510
rajatssaigal-cyber <rajatssaigal@gmail.com>
2026-01-08 13:43:16 +0530

    Update main.jsx

--- fire-calc-app/src/main.jsx
+++ fire-calc-app/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import FireCalcPro from './FireCalcPro.jsx'
 import './index.css'
+import { AuthProvider } from './contexts/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FireCalcPro />
+    <AuthProvider>
+      <FireCalcPro />
+    </AuthProvider>
   </React.StrictMode>,
 )
